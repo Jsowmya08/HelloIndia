d92466b9eaf8d96703731f72254e3d93432bc89c
Jsowmya08 <sowmya.jangam0807@gmail.com>
2021-10-04 22:15:46 -0700

    first commit

---
+++ helloindia/helloindia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloindia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello India!");
+        }
+    }
+}
